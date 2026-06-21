5a3045a2239073a0c6920298ff2ff1868de1482f
gad-biton <gadb633@gmail.com>
2026-06-21 14:58:29 +0300

    added temperature

--- conditions.py
+++ conditions.py
@@ -1,6 +1,14 @@
+# Part 1
+# 1
 age = int(input("what your age? "))
 if age >= 18:
     print("can enter")
 else:
     print("cannot enter")
-        
+# 2
+temperature = 38.2
+if temperature > 37.5:
+    print("high temperature")
+else:
+    print("normal temperature")
+                 
